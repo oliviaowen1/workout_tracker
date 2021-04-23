45214b1ccc1b5250e49cffe910e9808da1fa5e94
Olivia Owen <olivia.owen@hotmail.com>
2021-04-23 22:10:48 +0100

    Added routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,53 @@
+const db = require("../models");
+
+module.exports = function(app) {
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+
+    app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.findByIdAndUpdate(
+            req.params.id,
+            {
+                $push: {
+                    exercises: req.body
+                }
+            }
+        )
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+    
+    app.post("/api/workouts", ({ body }, res) => {
+        db.Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+    
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.find({})
+        .then(dbUser => {
+            res.json(dbUser);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+};
