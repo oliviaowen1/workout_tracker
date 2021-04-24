d18f67348422d923d4ba8917061bab4c915cfa50
Olivia Owen <olivia.owen@hotmail.com>
2021-04-24 20:09:49 +0100

    Included require package in server file

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const logger = require("morgan");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -19,7 +22,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useCreateIndex: true,
 });
 
-// Routes
 app.use(routes);
 
 app.listen(PORT, () => {
